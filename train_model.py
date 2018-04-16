c60f48b4c294c9c65889c3f4d0b7c52c7603bacf
dangitstam <dangt7@uw.edu>
2018-04-15 18:51:10 -0700

    train_model now incorporates truncated backpropagation through time

--- train_model.py
+++ train_model.py
@@ -2,13 +2,17 @@ import argparse
 import logging
 import os
 import shutil
+from tqdm import tqdm
 import sys
 
 import torch
+from torch.autograd import Variable
 from torch import optim
+from torch.nn.functional import cross_entropy
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
-from topic_rnn_rc.models.topic_rnn import TopicRNN
+from Dictionary import Corpus, word_vector_from_seq
+from topic_rnn_rc.models.rnn import RNN
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +29,7 @@ TODO:
 """
 
 MODEL_TYPES = {
-    "topic": TopicRNN
+    "vanilla": RNN
 }
 
 
@@ -34,45 +38,45 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     project_root = os.path.abspath(os.path.realpath(os.path.join(
         os.path.dirname(os.path.realpath(__file__)))))
-    parser.add_argument("--conflict-train-path", type=str,
+    parser.add_argument("--conflicts-train-path", type=str,
                         default=os.path.join(
-                            project_root, "conflicts", "train"),
-                        help="Path to the conflict training data.")
-    parser.add_argument("--conflict-dev-path", type=str,
+                            project_root, "data", "train"),
+                        help="Path to the Conflict Wikipedia JSON"
+                             "training data.")
+    parser.add_argument("--conflicts-dev-path", type=str,
                         default=os.path.join(
-                            project_root, "conflicts", "validation"),
-                        help="Path to the conflicts dev data.")
-    parser.add_argument("--conflict-test-path", type=str,
+                            project_root, "data", "validation"),
+                        help="Path to the Conflict Wikipedia JSON"
+                             "dev data.")
+    parser.add_argument("--conflicts-test-path", type=str,
                         default=os.path.join(
-                            project_root, "conflicts", "test"),
-                        help="Path to the conflicts test data.")
-    parser.add_argument("--load-path", type=str,
-                        help=("Path to load a saved model from and "
-                              "evaluate on test data. May not be "
-                              "used with --save-dir."))
+                            project_root, "data", "test"),
+                        help="Path to the Conflict Wikipedia JSON"
+                             "dev data.")
     parser.add_argument("--save-dir", type=str,
                         help=("Path to save model checkpoints and logs. "
                               "Required if not using --load-path. "
                               "May not be used with --load-path."))
     parser.add_argument("--model-type", type=str, default="topic-rnn",
-                        choices=["topic"],
+                        choices=["vanilla"],
                         help="Model type to train.")
     parser.add_argument("--min-token-count", type=int, default=10,
                         help=("Number of times a token must be observed "
                               "in order to include it in the vocabulary."))
-    parser.add_argument("--max-passage-length", type=int, default=150,
-                        help="Maximum number of words in the passage.")
-    parser.add_argument("--max-question-length", type=int, default=15,
-                        help="Maximum number of words in the question.")
-    parser.add_argument("--batch-size", type=int, default=64,
+    parser.add_argument("--bptt-limit", type=int, default=50,
+                        help="Extent in which the model is allowed to"
+                             "backpropagate.")
+    parser.add_argument("--batch-size", type=int, default=1,
                         help="Batch size to use in training and evaluation.")
     parser.add_argument("--hidden-size", type=int, default=256,
-                        help="Hidden size to use in RNN and Attention models.")
+                        help="Hidden size to use in RNN and TopicRNN models.")
+    parser.add_argument("--embedding-size", type=int, default=50,
+                        help="Embedding size to use in RNN and TopicRNN models.")
     parser.add_argument("--num-epochs", type=int, default=25,
                         help="Number of epochs to train for.")
     parser.add_argument("--dropout", type=float, default=0.2,
                         help="Dropout proportion.")
-    parser.add_argument("--lr", type=float, default=0.5,
+    parser.add_argument("--lr", type=float, default=0.005,
                         help="The learning rate to use.")
     parser.add_argument("--log-period", type=int, default=50,
                         help=("Update training metrics every "
@@ -95,22 +99,101 @@ def main():
         else:
             torch.cuda.manual_seed(args.seed)
 
+    # Construct vocabulary
+    corpus = Corpus()
+
+    if not args.conflicts_train_path:
+        raise ValueError("Training data directory required")
+
+    # TODO: Make a vocabulary that restricts the vocab size.
+    training_files = os.listdir(args.conflicts_train_path)
+
+    print("Building corpus from Conflict Wikipedia JSON files:")
+    for file in tqdm(training_files):
+        # Corpus expects a full file path.
+        corpus.add_document(os.path.join(args.conflicts_train_path, file))
+
+    vocab_size = len(corpus.dictionary)
+    print("VOCAB SIZE:", vocab_size)
 
     # Create model of the correct type.
-    if args.model_type == "topic":
-        logger.info("Building TopicRNN model")
-        model = TopicRNN(embedding_matrix, args.hidden_size, args.dropout)
+    print("Elman RNN model --------------")
+    logger.info("Building Elman RNN model")
+    model = RNN(vocab_size, args.embedding_size, args.hidden_size,
+                args.batch_size, layers=2, dropout=args.dropout)
+
+    if args.cuda:
+        model.cuda()
 
     logger.info(model)
 
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-def train_epoch(model):
+    try:
+        train_epoch(model, corpus, args.batch_size, args.bptt_limit, optimizer,
+                    args.cuda)
+    except KeyboardInterrupt:
+        pass
+
+    print()  # Printing in-place progress flushes standard out.
+
+
+def train_epoch(model, corpus, batch_size, bptt_limit, optimizer, cuda):
     """
     Train the model for one epoch.
     """
 
     # Set model to training mode (activates dropout and other things).
     model.train()
+    print("Training in progress:")
+    for i, document in enumerate(corpus.documents):
+        # Incorporation of time requires feeding in by one word at
+        # a time.
+        #
+        # Iterate through the words of the document, calculating loss between
+        # the current word and the next, from first to penultimate.
+
+        for j, section in enumerate(document["sections"]):
+            loss = 0
+            hidden = model.init_hidden()
+
+            # Training at the word level allows flexibility in inference.
+            for k in range(section.size(0) - 1):
+                current_word = word_vector_from_seq(section, k)
+                next_word = word_vector_from_seq(section, k + 1)
+
+                if cuda:
+                    current_word = current_word.cuda()
+                    next_word = next_word.cuda()
+
+                output, hidden = model(Variable(current_word), hidden)
+
+                # Calculate loss between the next word and what was anticipated.
+                loss += cross_entropy(output.view(1, -1), Variable(next_word))
+
+                # Perform backpropagation and update parameters.
+                #
+                # Detaches hidden state history to prevent bp all the way
+                # back to the start of the section.
+                if (k + 1) % bptt_limit == 0:
+                    optimizer.zero_grad()
+                    loss.backward(retain_graph=True)
+                    optimizer.step()
+
+                    # Print progress
+                    print_progress_in_place("Document #:", i,
+                                            "Section:", j,
+                                            "Word:", k,
+                                            "Normalized BPTT Loss:",
+                                            loss.data[0] / bptt_limit)
+
+                    loss = 0
+                    hidden = Variable(hidden.data)
+
+
+def print_progress_in_place(*args):
+    print("\r", *args, end="")
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
